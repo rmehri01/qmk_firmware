04dbc4158fcf0125dcff00eebbe488441bbd4bd9
Ryan Mehri <ryan.mehri1@gmail.com>
2022-04-29 23:32:31 -0700

    modify gaming layer

--- keyboards/crkbd/keymaps/rmehri01/keymap.c
+++ keyboards/crkbd/keymaps/rmehri01/keymap.c
@@ -100,11 +100,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [GAM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      OS_CTRL,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+      OS_CTRL,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_Y,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      OS_SHFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,  KC_SPC,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,GAMING_OFF,
+       KC_ESC,    KC_Z,    KC_X,  KC_TAB,  KC_SPC,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,GAMING_OFF,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                              KC_1,    KC_2,    KC_3,    _______,  KC_SPC, OS_SHFT
                                       //`--------------------------'  `--------------------------'
